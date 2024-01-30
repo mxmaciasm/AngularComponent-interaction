da00db8918c60c7f35e2bf7ba6b9e83d66f9a4c5
mervyn.macias <mervyn.macias@bmachala.com>
2024-01-30 16:52:43 -0500

    local

--- src/app/hero-child.component.ts
+++ src/app/hero-child.component.ts
@@ -4,12 +4,17 @@ import { Hero } from './hero';
 
 @Component({
   selector: 'app-hero-child',
+
+  //Pass data from parent to child with input binding
+  
   template: `
     <h3>{{hero.name}} says:</h3>
     <p>I, {{hero.name}}, am at your service, {{masterName}}.</p>
   `
 })
 export class HeroChildComponent {
+  //https://angular.io/guide/inputs-outputs#input
+
   @Input() hero!: Hero;
-  @Input('master') masterName = '';
+  @Input('masteralias') masterName = ''; //tiene un alias 'masteralias'
 }
--- src/app/hero-parent.component.ts
+++ src/app/hero-parent.component.ts
@@ -4,13 +4,17 @@ import { HEROES } from './hero';
 
 @Component({
   selector: 'app-hero-parent',
+  
+  // HeroParentComponent anida el HeroChildComponent "app-hero-child" secundario dentro de un repetidor *ngFor, 
+  //vinculando su propiedad de cadena master al alias master del child y la instancia de heroe de cada iteración a la propiedad de heroe child.  
+  //heroes y master es la propiedad de HeroParentComponent. Para acceder y pasar un heroe se usa ngFor a heroes, 
   template: `
     <h2>{{master}} controls {{heroes.length}} heroes</h2>
 
     <app-hero-child
       *ngFor="let hero of heroes"
       [hero]="hero"
-      [master]="master">
+      [masteralias]="master">
     </app-hero-child>
   `
 })
